2ef726ddeb28e194f0128fe25fa8538486ebd91c
Ben <shatterwaltz@gmail.com>
2017-10-14 16:14:58 -0400

    Edits/Loads from SharedPreferences
    When pressing button1, 2, or 3, now saves which one you pressed and
    loads on next app use.

--- app/src/main/java/sawari/myfirstapp/DisplayMessageBottomFragment.java
+++ app/src/main/java/sawari/myfirstapp/DisplayMessageBottomFragment.java
@@ -1,6 +1,8 @@
 package sawari.myfirstapp;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -25,9 +27,15 @@ public class DisplayMessageBottomFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        //get message from string as well as view instance and preferences
         final String message = getArguments().getString(MainActivity.EXTRA_MESSAGE);
         final View view = inflater.inflate(R.layout.fragment_display_message_bottom, container, false);
+        final SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        //Set button pressed message to whatever is saved, if any
+        ((TextView)view.findViewById(R.id.buttonPressedText)).setText(preferences.getString("lastButtonPressed", ""));
+
+        //listener that will update text to display which button was clicked
         View.OnClickListener listener = new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -35,14 +43,21 @@ public class DisplayMessageBottomFragment extends Fragment {
                 TextView buttonText = ((TextView)view.findViewById(R.id.buttonPressedText));
                 text.setText(message);
                 buttonText.setText("You have pressed "+((Button)v).getText());
+                //Save this message to preferences upon pressing a button
+                preferences.edit().putString("lastButtonPressed", "You have pressed "+((Button)v).getText()).commit();
             }
         };
+
+        //set listener to each of the buttons
         Button button1 = (Button)view.findViewById(R.id.button1);
         button1.setOnClickListener(listener);
         Button button2 = (Button)view.findViewById(R.id.button2);
         button2.setOnClickListener(listener);
         Button button3 = (Button)view.findViewById(R.id.button3);
         button3.setOnClickListener(listener);
+
+
+
         return view;
     }
 
